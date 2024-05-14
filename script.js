caadabc0562f8a355891e37bccbcd4f88dc09bb8
cynophilist <84233234+adtt123@users.noreply.github.com>
2024-05-14 21:17:42 +0000

    Made the first changes to story!

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Declare variables below to save the different divs of your story.
-
-
-
-
-
+let beginning = document.querySelector(".storyopening")
+let option1screen = document.querySelector(".option-one-screen")
+let option2screen = document.querySelector(".option-two-screen")
+let option1end = document.querySelector(".option-one-end")
+let option2end = document.querySelector(".option-two-end")
 
 
 
